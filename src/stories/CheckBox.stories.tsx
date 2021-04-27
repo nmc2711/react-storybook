7bb74c452de78aae4ac2bb6b25ef775233d45b7a
sanghan <nmc2711@naver.com>
2021-04-27 16:29:19 +0900

    all push storybook commit

---
+++ src/stories/CheckBox.stories.tsx
@@ -0,0 +1,145 @@
+import React, { useState, useEffect, useCallback } from "react";
+import CheckBox from "./CheckBox";
+
+export default {
+  title: "Custom-UI/Checkbox",
+  component: CheckBox,
+};
+
+export const Default = () => {
+  const [check, setCheck] = useState<boolean>(false);
+
+  const changeCheckStatus = useCallback(() => {
+    const checked = !check;
+
+    setCheck(checked);
+  }, [check]);
+
+  return <CheckBox status={check} callback={changeCheckStatus} />;
+};
+
+export const MultipleCheckBox = () => {
+  type exampleType = {
+    id: number;
+    checked: boolean;
+    value: string;
+  };
+
+  const initData: Array<exampleType> = [
+    { id: 1, checked: false, value: "one" },
+    { id: 2, checked: false, value: "two" },
+    { id: 3, checked: false, value: "three" },
+    { id: 4, checked: false, value: "four" },
+  ];
+
+  const [allCheck, setAllCheck] = useState<boolean>(false);
+  const [example, setExample] = useState<Array<exampleType>>(initData);
+
+  const allCheckCallback = () => {
+    const checked = allCheck === true ? false : true;
+
+    const copyForAllCheck = [...example];
+
+    // change for check state
+
+    copyForAllCheck.map((item) => {
+      item.checked = !item.checked;
+    });
+
+    setAllCheck(checked);
+
+    setExample(copyForAllCheck);
+  };
+
+  const multifulCheckCallback = (value: any) => {
+    if (value !== undefined) {
+      // 해당 체크박스 value
+      const thisCheckbox = example.find((ex) => ex.value === value);
+      // 해당 체크 박스를 제외한 나머지 항목
+      const otherCheckbox = example.filter((ex) => ex.value != value);
+
+      if (thisCheckbox !== undefined) {
+        const multipleCheck = [
+          ...otherCheckbox,
+          { id: thisCheckbox.id, checked: !thisCheckbox.checked, value: value },
+        ];
+
+        multipleCheck.sort(function (a, b) {
+          return a.id < b.id ? -1 : 1;
+        });
+
+        let allState = true;
+
+        for (let index = 0; index < multipleCheck.length; index++) {
+          if (multipleCheck[index].checked === false) allState = false;
+        }
+        setAllCheck(allState);
+
+        setExample([...multipleCheck]);
+      }
+    }
+  };
+
+  return (
+    <>
+      <div>
+        <div>all checkbox</div>
+        <div>
+          <CheckBox status={allCheck} callback={allCheckCallback} />
+        </div>
+        <div>checkbox</div>
+      </div>
+      {example.map((data, idx) => {
+        const { checked } = data;
+        return (
+          <CheckBox
+            key={`checkbox-${idx}`}
+            status={checked}
+            callback={() => multifulCheckCallback(data.value)}
+          />
+        );
+      })}
+    </>
+  );
+};
+
+export const CheckSize = () => {
+  const [check, setCheck] = useState<boolean>(false);
+
+  const changeCheckStatus = useCallback(() => {
+    const checked = check === true ? false : true;
+    setCheck(checked);
+  }, [check]);
+
+  return <CheckBox status={check} callback={changeCheckStatus} size={50} />;
+};
+
+export const BackgroundColor = () => {
+  const [check, setCheck] = useState<boolean>(false);
+
+  const changeCheckStatus = useCallback(() => {
+    const checked = check === true ? false : true;
+    setCheck(checked);
+  }, [check]);
+
+  return (
+    <CheckBox status={check} callback={changeCheckStatus} bgColor="#fff000" />
+  );
+};
+
+export const LabelControl = () => {
+  const [check, setCheck] = useState<boolean>(false);
+
+  const changeCheckStatus = useCallback(() => {
+    const checked = check === true ? false : true;
+    setCheck(checked);
+  }, [check]);
+
+  return (
+    <CheckBox
+      status={check}
+      callback={changeCheckStatus}
+      label={{ text: "라벨테스트", id: "labeltest", position: "right" }}
+    />
+  );
+};
---
+++ src/stories/CheckBox.tsx
@@ -0,0 +1,135 @@
+import React from "react";
+
+import styled from "styled-components";
+
+export interface ICheckBoxProps {
+  /**
+   *  checked status
+   */
+  status: boolean;
+
+  /**
+   *  onchange event handler
+   */
+  callback?: () => void;
+
+  /**
+   * checkbox size
+   */
+  size?: number;
+
+  /**
+   *  checked background color
+   */
+  bgColor?: string;
+
+  /**
+   *  check handler label
+   */
+  label?: {
+    text: string;
+    id: string;
+    position: "right" | "left";
+  };
+}
+
+export default function CustomCheckBox(props: ICheckBoxProps) {
+  const { status, callback, size, bgColor, label } = props;
+  return (
+    <CheckboxContainer>
+      <CheckBox
+        id={label && label.id}
+        status={status}
+        type="checkbox"
+        className={`${status === true && "on"}`}
+        onChange={callback}
+        size={size}
+        bgColor={bgColor}
+      />
+      {label && (
+        <label htmlFor={label.id} className={`${label.position}`}>
+          {label.text}
+        </label>
+      )}
+    </CheckboxContainer>
+  );
+}
+// styled
+const CheckboxContainer = styled.div`
+  display: inline-flex;
+  align-items: center;
+  width: fit-content;
+  & > label {
+    cursor: pointer;
+    &.left {
+      float: left;
+      margin-right: 8px;
+    }
+    &.right {
+      float: right;
+      margin-left: 8px;
+    }
+  }
+`;
+const CheckBox = styled.input`
+  position: relative;
+  width: ${(props: ICheckBoxProps) =>
+    props.size ? `${props.size}px` : "24px"};
+  height: ${(props: ICheckBoxProps) =>
+    props.size ? `${props.size}px` : "24px"};
+  background-color: #fff;
+  border: 1px solid lightgray;
+  border-radius: 4px;
+  appearance: none;
+  outline: none;
+  cursor: pointer;
+  border-radius: 4px;
+  overflow: hidden;
+  -webkit-appearance: none;
+  -moz-appearance: none;
+  appearance: none;
+
+  &::before {
+    content: "";
+    position: absolute;
+    width: 13%;
+    height: 50%;
+    top: 28%;
+    left: 55%;
+    background-color: #e0e0e0;
+    -ms-transform: rotate(45deg); /* IE 9 */
+    -webkit-transform: rotate(45deg); /* Chrome, Safari, Opera */
+    transform: rotate(45deg);
+    border-radius: 10px;
+  }
+  &::after {
+    content: "";
+    position: absolute;
+    width: 40%;
+    height: 13%;
+    background-color: #e0e0e0;
+    top: 50%;
+    left: 17%;
+    -ms-transform: rotate(45deg); /* IE 9 */
+    -webkit-transform: rotate(45deg); /* Chrome, Safari, Opera */
+    transform: rotate(45deg);
+    border-radius: 10px;
+  }
+  &.on {
+    border-color: ${(props: ICheckBoxProps) =>
+      props.bgColor ? `${props.bgColor}` : "#632beb"};
+    background-color: ${(props: ICheckBoxProps) =>
+      props.bgColor ? `${props.bgColor}` : "#632beb"};
+    transition: 0.2s all ease 0s;
+    &::before,
+    &::after {
+      background-color: #fff;
+    }
+  }
+`;
+// default state checkbox
+CustomCheckBox.defaultProps = {
+  status: false,
+  size: 24,
+  backgroundColor: "#632beb",
+};
