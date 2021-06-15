5ceac0a094eacb03542dcead24f8730328d21456
hwangsanghan <nmc2711@naver.com>
2021-06-15 09:53:25 +0900

    feat: kakao map

---
+++ src/stories/KakaoMap.stories.tsx
@@ -0,0 +1,10 @@
+import React, { useState, useEffect, useCallback } from "react";
+import KakaoMap from "./ui/kakaoMap/KakaoMap";
+
+export default {
+  title: "Custom-UI/KakaoMap",
+};
+
+export const Default = () => {
+  return <KakaoMap />;
+};
---
+++ src/stories/ui/kakaoMap/KakaoMap.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function KakaoMap() {
+  return <div>카카오맵</div>;
+}
+
+export default KakaoMap;
