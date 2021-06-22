546a7c89652c08190432392b2b6cff932bcaf8c0
hwangsanghan <nmc2711@naver.com>
2021-06-22 09:24:15 +0900

    feat : dnd plus

---
+++ src/stories/Dnd.stories.tsx
@@ -0,0 +1,78 @@
+import React, { useState, useRef } from "react";
+import useDnd from "./ui/dnd";
+
+export default {
+  title: "Custom-UI/Dnd",
+};
+
+export const Default = () => {
+  const draggingRef = useRef();
+  const dragoverRef = useRef();
+  const [list, setList] = useState([
+    "5678",
+    "테스트",
+    "테스트2123",
+    "테스트5689",
+    "어디?",
+    "저기?",
+  ]);
+  const { handleDragStart, handleDragEnter, handleDragOver } = useDnd({
+    list,
+    setList,
+    draggingRef,
+    dragoverRef,
+  });
+  return (
+    <div style={{ margin: "60px 60px" }}>
+      {list &&
+        list.map((item, key) => {
+          return (
+            <div
+              onDragStart={(e) => handleDragStart(e, key)}
+              onDragOver={(e) => e.preventDefault()}
+              onDrop={(e) => handleDragEnter(e, key)}
+              draggable
+            >
+              {item}
+            </div>
+          );
+        })}
+    </div>
+  );
+};
+
+export const Dragging = () => {
+  const draggingRef = useRef();
+  const dragoverRef = useRef();
+  const [list, setList] = useState([
+    "5678",
+    "테스트",
+    "테스트2123",
+    "테스트5689",
+    "어디?",
+    "저기?",
+  ]);
+  const { handleDragStart, handleDragEnter, handleDragOver } = useDnd({
+    list,
+    setList,
+    draggingRef,
+    dragoverRef,
+  });
+  return (
+    <div style={{ margin: "60px 60px" }}>
+      {list &&
+        list.map((item, key) => {
+          return (
+            <div
+              onDragStart={(e) => handleDragStart(e, key)}
+              onDragOver={(e) => e.preventDefault()}
+              onDragEnter={(e) => handleDragEnter(e, key)}
+              draggable
+            >
+              {item}
+            </div>
+          );
+        })}
+    </div>
+  );
+};
--- src/stories/ui/dnd/index.tsx
+++ src/stories/ui/dnd/index.tsx
@@ -1,7 +1,55 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+/***
+ * @title : 드래그앤 드롭
+ * @autor : 황상한
+ * @pattern :
+ * import useDnd from 'dnd';
+ * const draggingRef = useRef();
+   const dragoverRef = useRef();
+   const [list, setList] = useState(['5678','테스트','테스트2123','테스트5689', '어디?', '저기?']);
+   const { handleDragStart, handleDragEnter, handleDragOver } = useDnd({ list, setList, draggingRef, dragoverRef });
+ *  <div
+     onDragStart={(e) => handleDragStart(e, key)} -- 필수
+     onDragOver={(e) => e.preventDefault()} -- 이벤트 버블 방지(필수)
+     onDragEnter?={(e) => handleDragEnter(e, key)} --- 드래그하는 동시에 동적인 변경(선택)
+     onDrop?={(e) => handleDragEnter(e, key)} --- 드랍하는 순간에만 변경(선택)
+     draggable -- 필수
+     >
+ */
 
-function DnDComponent() {
-  return <div></div>;
+export interface DndProps {
+  list: Array<String>;
+  setList: (value: String[]) => void;
+  draggingRef: React.MutableRefObject<any>;
+  dragoverRef: React.MutableRefObject<any>;
 }
 
-export default DnDComponent;
+export default function DragDropComponent(props: DndProps) {
+  const { list, setList, draggingRef, dragoverRef } = props;
+  const handleDragStart = (e: any, position: number) => {
+    // DND할 아이템의 인덱스 위치를 기억
+    draggingRef.current = position;
+  };
+
+  const handleDragEnter = (
+    e: React.DragEvent<HTMLDivElement>,
+    position: number
+  ) => {
+    // DND 위치값을 지정
+    dragoverRef.current = position;
+    const listCopy = [...list];
+    const draggingRefContent = listCopy[draggingRef.current];
+    listCopy.splice(draggingRef.current, 1);
+    listCopy.splice(dragoverRef.current, 0, draggingRefContent);
+
+    draggingRef.current = dragoverRef.current;
+    dragoverRef.current = null;
+    setList(listCopy);
+  };
+
+  const handleDragOver = (e: any) => {
+    e.preventDefalut();
+  };
+
+  return { handleDragStart, handleDragEnter, handleDragOver };
+}
